f286b1af93b939d437bdca48c36e648bd0f3c811
Siddhant <siddhantbokil27@gmail.com>
2026-08-13 22:24:26 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+
+  char command[30];
+  scanf("%s", command);
+
+  printf("%s: command not found", command);
 
   return 0;
 }
