8b377acd9bda3ff519a502d2c1003eeaad241480
Siddhant <siddhantbokil27@gmail.com>
2026-08-17 21:48:58 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
+char* builtin_words[] = {
+  "echo",
+  "type",
+  "exit"
+};
+
+int builtin_words_count = sizeof(builtin_words) / sizeof(builtin_words[0]);
+
+void parseCommand(char* command, char* rest)
+{
+    
+    // Handle echo
+    if (strcmp(command, "echo") == 0)
+    {
+      printf("%s\n", rest);
+      return;
+    }
+    
+    // Handle exit
+    if (strcmp(command, "exit") == 0)
+    {
+      exit(EXIT_SUCCESS);
+    }
+
+    // Handle type
+    if (strcmp(command, "type") == 0)
+    {
+      for(int i = 0; i < builtin_words_count; i++)
+      {
+        if(strcmp(builtin_words[i], rest)==0)
+        {
+          printf("%s is a shell builtin\n", rest);
+          return;
+        }
+      }
+      printf("%s: not found\n", rest);
+      return;
+    }
+    printf("%s: command not found\n", command);
+}
+
+void parseText(char* text)
+{
+    int command_size = 0;
+    char command[20];
+    
+    while (text[command_size] != ' ' && text[command_size] != '\0')
+    {
+      command_size++;
+    }
+    
+    memcpy(command, text, command_size);
+    command[command_size] = '\0';
+
+    char* rest = text + command_size + 1;
+
+    while(* rest == ' ')
+    {
+      rest++;
+    }
+    
+    parseCommand(command, rest);
+}
+
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
@@ -10,34 +75,15 @@ int main(int argc, char *argv[])
   // TODO: Uncomment the code below to pass the first stage
 
   char text[100];
-  char command[20];
 
   while (1)
   {
     printf("$ ");
     fgets(text, sizeof(text), stdin);
-    int i = 0;
+    text[strcspn(text, "\n")] = '\0';
+    
+    parseText(text);
 
-    while (text[i] != ' ' && text[i] != '\n' && text[i] != '\0')
-    {
-      i++;
-    }
-
-    memcpy(command, text, i);
-    command[i] = '\0';
-
-    if (strcmp(command, "echo") == 0)
-    {
-      printf("%s", text + i + 1);
-      continue;
-    }
-
-    if (strcmp(command, "exit") == 0)
-    {
-      return 0;
-    }
-
-    printf("%s: command not found\n", command);
   }
 
   return 0;
